57210a66993a8a0bb8f81613032e46f9f908f256
Jdpower <Jdpower@DESKTOP-J0NH54S>
2017-03-20 23:46:02 -0400

    Nouvelle branch de la NomFeature

--- AgendaDemo/AgendaDemo/Program.cs
+++ AgendaDemo/AgendaDemo/Program.cs
@@ -14,6 +14,7 @@ namespace AgendaDemo
         [STAThread]
         static void Main()
         {
+            //modification demo sur la branch
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
